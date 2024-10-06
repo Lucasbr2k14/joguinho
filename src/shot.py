2e2343efdb5c84b821f6d1d142c73993d078e5df
lucas raubach <lucasbraubach@gmail.com>
2024-10-06 10:50:37 -0300

    Aceitando alteraçoes :>

--- src/shot.py
+++ src/shot.py
@@ -6,17 +6,9 @@ y = 1
 class Shot:
 
     def __init__(self, screen:Surface):
-<<<<<<< HEAD
         
         self.radius   = 5
         self.velocity = 4
-=======
-
-        self.radius        = 5
-        self.velocity      = 4
-        self.cooldown      = 3
-        self.cooldown_cout = 0
->>>>>>> c7d9a81 (commit to pull)
         self.screen   = screen
         self.shots    = []
         
@@ -30,7 +22,6 @@ class Shot:
         
             self.next_shot = time + self.cooldown_time
 
-<<<<<<< HEAD
             hyp_len = pow(pow(player_pos[x] - mouse_pos[x], 2) + pow(player_pos[y] - mouse_pos[y], 2), 1/2) 
 
             sin = (mouse_pos[x] - player_pos[x]) / hyp_len
@@ -44,8 +35,6 @@ class Shot:
 
     def destroy(self, shot:int):
        self.shots.pop(shot) 
-=======
->>>>>>> c7d9a81 (commit to pull)
 
     def check_condidion_of_existence(self):
 
@@ -74,17 +63,9 @@ class Shot:
     def get_cooldown(self, time:int):
         return self.next_shot <= time
 
-
     def draw(self):
 
         if len(self.shots) > 0:
             for i in range(len(self.shots)):
-<<<<<<< HEAD
                 draw.circle(self.screen, 0xFFFFFF, self.shots[i]["posi"], self.radius)
-=======
-                draw.circle(self.screen, "white", self.shots[i]["posi"], self.radius)
-    
-    def destroy(self, shot:int):
-        self.shots.pop(shot) 
->>>>>>> c7d9a81 (commit to pull)
     
