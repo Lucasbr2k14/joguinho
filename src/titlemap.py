d7c8d15c128260abdaa008a4bf38b5a959602d4b
lucas raubach <lucasbraubach@gmail.com>
2024-10-11 02:13:24 -0300

    Optimizing loops 🫠

--- src/titlemap.py
+++ src/titlemap.py
@@ -1,17 +1,20 @@
 from pygame import Surface, image, Rect, transform
+from time import sleep
+
 
 class TileMap:
 
     def __init__(self, screen:Surface):
+        
         self.screen = screen
-
         self.mul    = 4
         self.len    = 16
         self.matrix = []
-    
+        self.tile   = []
+
         self.load_visuals()
         self.load_map()
-
+        self.cache_tilemap()
 
     def load_visuals(self):
         self.visuals = image.load_extended("./Sprites/land.png")
@@ -27,14 +30,20 @@ class TileMap:
                     linha[i] = int(linha[i])
                 
                 self.matrix.append(linha)
-    
+
+    def cache_tilemap(self):
+        for i in range(400+1):
+            tile_x = 16 * self.mul * i % 320
+            tile_y = i // 20 * 16 * self.mul
+            rect = ((tile_x, tile_y),(16*self.mul,16*self.mul))
+            self.tile.append(rect)
+
     def draw(self):
-    
-        for l in range(len(self.matrix)):
-            for c in range(len(self.matrix[l])):
-
-                tile_x = ((0*self.mul)+(16*self.mul)*self.matrix[l][c]) % 320
-                tile_y = 0*self.mul + (self.matrix[l][c]//20)*(16*self.mul)
-
-                rect = Rect((tile_x, tile_y),(16*self.mul,16*self.mul))
-                self.screen.blit(self.visuals, (c*self.len*self.mul, (l*self.len*self.mul)), rect)
+        tile_size = self.len*self.mul
+        for i in range(len(self.matrix) * len(self.matrix[0])):
+            l = i // 20
+            c = i % 20
+            x = c * tile_size
+            y = l * tile_size
+            index_tile = self.matrix[l][c]
+            self.screen.blit(self.visuals, (x, y), self.tile[index_tile])
