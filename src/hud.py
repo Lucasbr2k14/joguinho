785f7b69970535122a11bb4bade8db2572775970
lucas raubach <lucasbraubach@gmail.com>
2024-10-28 12:07:42 -0300

    Agora a vida no lugar certo :)

--- src/hud.py
+++ src/hud.py
@@ -3,30 +3,23 @@ from pygame import Surface,draw,image
 class Hud:
 
     def __init__(self , surface:Surface):
-        self.mana = 100
-        self.life = 100
+        self.life:int   = 0
+        self.mana:float = 0.0
 
-        self.surface = surface
+        self.screen = surface
         
-        self.surface = surface
+        self.screen = surface
         self.load_visuals()
         
     def load_visuals(self):
         self.sprites = image.load_extended("./Sprites/teste.png")
         
     def draw(self):
-        draw.rect(self.surface,'red',[(0,0),(self.life,16)])
-        draw.rect(self.surface,'blue',[(0,18),(self.mana,16)])
-        self.surface.blit(self.sprites,[0,0],[[0,432],[101,16]])
-        self.surface.blit(self.sprites,[0,18],[[0,432],[101,16]])
+        draw.rect(self.screen,'red',[(0,0),(self.life,16)])
+        draw.rect(self.screen,'blue',[(0,18),(self.mana,16)])
+        self.screen.blit(self.sprites,[0,0],[[0,432],[101,16]])
+        self.screen.blit(self.sprites,[0,18],[[0,432],[101,16]])
         
-
-    def new_mana(self):
-        self.mana -= 10
-    
-    def recharge_mana(self):
-        if self.mana < 100:
-            self.mana += 0.05
-            
-    def return_mana(self):
-        return self.mana
+    def pull_from_player(self, player:tuple[int, float]):
+        self.life = player[0]
+        self.mana = player[1]
